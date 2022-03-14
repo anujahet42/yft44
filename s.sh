1e9ccad80e89ce690b881b9f70c385058744265f
anujahet42 <101571146+anujahet42@users.noreply.github.com>
2022-03-14 19:39:33 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo bash -c "$(curl -fsSL https://lolcollab.teleport.sh/scripts/94f3123402996f4cabe3bef3c0cf0f42/install-node.sh)"
+bash -c "$(curl -fsSL https://lolcollab.teleport.sh/scripts/94f3123402996f4cabe3bef3c0cf0f42/install-node.sh)"
 while [ 1 ]; do
 sleep 3
 done
